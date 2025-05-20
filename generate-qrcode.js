311b31c7e288fff89349629960259ca5712376c9
adrianalima99 <adriana.slima0899@gmail.com>
2025-05-20 11:27:30 -0300

    Corrigindo QR Code

--- generate-qrcode.js
+++ generate-qrcode.js
@@ -1,6 +1,6 @@
 const QRCode = require('qrcode');
 
-QRCode.toFile('./assets/qrcode.png', 'https://seu-link.com', {
+QRCode.toFile('./assets/qrcode.png', 'https://github.com/adrianalima99/qrcode-especial', {
     color: {
         dark: '#0A2463',  // Azul escuro
         light: '#D6F1FF'  // Fundo azul claro
